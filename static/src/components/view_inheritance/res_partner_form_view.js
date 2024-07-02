b312dddf6d9c0e161f56219a7474962f8f5b0c75
Nidal SJ <connectnidalsj@gmail.com>
2024-07-02 16:48:04 +0530

    kanban and form view

---
+++ static/src/components/view_inheritance/res_partner_form_view.js
@@ -0,0 +1,26 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { formView } from "@web/views/form/form_view";
+import { FormController } from "@web/views/form/form_controller";
+import { useService } from "@web/core/utils/hooks";
+
+
+class ResPartnerFormController extends FormController { 
+    setup(){
+        super.setup()
+        console.log('res partner controller')
+        this.action = useService('action')
+    }
+
+
+}
+
+ResPartnerFormController.template = 'owl.ResPartnerFormView'
+
+export const resPartnerFormView = {
+    ...formView,
+    Controller: ResPartnerFormController,
+}
+
+registry.category('views').add('res_partner_form_view', resPartnerFormView)
--- static/src/components/view_inheritance/res_partner_kanban_view.js
+++ static/src/components/view_inheritance/res_partner_kanban_view.js
@@ -5,11 +5,17 @@ import { kanbanView } from "@web/views/kanban/kanban_view"
 import { KanbanController } from "@web/views/kanban/kanban_controller"
 import { useService } from "@web/core/utils/hooks"
 
+const { onWillStart } = owl
 
 class ResPartnerKanbanController extends KanbanController {
     setup(){
         super.setup()
         this.action = useService('action')
+        this.orm = useService('orm')
+
+        onWillStart(async ()=>{
+            this.customerLocations = await this.orm.readGroup('res.partner', [], ['state_id'], ['state_id'])
+        })
     }
 
     openSalesView(){
@@ -31,7 +37,21 @@ class ResPartnerKanbanController extends KanbanController {
         })
     }
 
+    selectLocations(state){
+        const id = state[0]
+        const name = state[1]
+
+        this.env.searchModel.setDomainParts({
+            state: {
+                domain: [['state_id', '=', id]],
+                facetLabel: name
+            }
+        })
+    }
+
 }
+
+ResPartnerKanbanController.template = 'owl.ResPartnerKanbanView'
 
 export const resPartnerKanbanView = {
     ...kanbanView,
