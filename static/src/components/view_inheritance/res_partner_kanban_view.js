cd2a05aa8acf3d0ad17de95393d31f8bf6b24bf7
Nidal SJ <connectnidalsj@gmail.com>
2024-07-03 17:14:02 +0530

    kanban and form

--- static/src/components/view_inheritance/res_partner_kanban_view.js
+++ static/src/components/view_inheritance/res_partner_kanban_view.js
@@ -6,47 +6,63 @@ import { KanbanController } from "@web/views/kanban/kanban_controller"
 import { useService } from "@web/core/utils/hooks"
 
 const { onWillStart } = owl
+const isFromContactsKanban = Symbol('isFromContactsKanban'); //defining this as a symbol - guaranteed to be unique
 
 class ResPartnerKanbanController extends KanbanController {
-    setup(){
+    setup() {
         super.setup()
         this.action = useService('action')
         this.orm = useService('orm')
 
-        onWillStart(async ()=>{
+        //fetching customer locations on start before displaying
+        onWillStart(async () => {
             this.customerLocations = await this.orm.readGroup('res.partner', [], ['state_id'], ['state_id'])
         })
     }
 
-    openSalesView(){
-        this.action.doAction({ // doAction - used to navigate to different views or executing specific actions
+    openSalesView() {
+        this.action.doAction({ // doAction - used to navigate to different views or executing actions
             type: 'ir.actions.act_window',
-            name:'Customer Sales',
-            res_model:'sale.order',
-            views:[[false,'list'],[false,'form']],
+            name: 'Customer Sales',
+            res_model: 'sale.order',
+            views: [[false, 'list'], [false, 'form']],
 
         })
     }
 
-    openInvoicesView(){
+    openInvoicesView() {
         this.action.doAction({
             type: 'ir.actions.act_window',
             name: 'Invoices',
             res_model: 'account.move',
-            views:[[false,'list'],[false,'form']],
+            views: [[false, 'list'], [false, 'form']],
         })
     }
 
-    selectLocations(state){
+    selectLocations(state) {
         const id = state[0]
         const name = state[1]
 
-        this.env.searchModel.setDomainParts({
-            state: {
-                domain: [['state_id', '=', id]],
-                facetLabel: name
+        // turn off filters - only one filter at a time
+        const customerFilters = this.env.searchModel.getSearchItems((searchItem) => {
+            console.log('filter currently using is:', searchItem);
+            searchItem[isFromContactsKanban];
+        });
+
+        for (const customerFilter of customerFilters) {
+            if (customerFilter.isActive) {
+                console.log('toggling off filter:', customerFilter);
+                this.env.searchModel.toggleSearchItem(customerFilter.id);
             }
-        })
+        }
+
+        // create new filter for location in search view
+        this.env.searchModel.createNewFilters([{
+            description: name,
+            domain: [["state_id", "=", id]],
+            [isFromContactsKanban]: true, // custom key to identify our filters
+        }]);
+
     }
 
 }
